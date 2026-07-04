2f6ecf39bcd2550428ba0189e687aed71506ecdf
Akshar Patel <akshar8460@gmail.com>
2026-07-05 00:09:09 +0530

    Lift search state to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <main>
       <Header
-       title="CineScope 🎬"
-       subtitle="Search your favourite movies"
-     />
-      <SearchBar />
+        title="CineScope 🎬"
+        subtitle="Search your favourite movies."
+      />
+
+      <SearchBar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+      />
+
+      <p>Searching for: {searchTerm}</p>
     </main>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,20 @@
-import {useState} from "react";
-
-function SearchBar() {
-    const [searchTerm, setSearchTerm] = useState("");
-
+function SearchBar({ searchTerm, setSearchTerm }) {
     function handleChange(event) {
-        setSearchTerm(event.target.value);
+      setSearchTerm(event.target.value);
     }
-
+  
     return (
-        <section>
-            <input 
-            type = "text" 
-            placeholder = "Search for a movie..." 
-            value = {searchTerm}
-            onChange = {handleChange}
-            />
-            <button> Search </button>
-        
-        <p>You typed: {searchTerm}</p>
-        </section>
+      <section>
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+  
+        <button>Search</button>
+      </section>
     );
-}
-
-export default SearchBar;
+  }
+  
+  export default SearchBar;
