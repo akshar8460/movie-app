bc4d89d0569796d05b0614e094f1290417b82f0a
Akshar Patel <akshar8460@gmail.com>
2026-07-04 23:37:00 +0530

    Make SearchBar a controlled component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,23 @@
+import {useState} from "react";
+
 function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    function handleChange(event) {
+        setSearchTerm(event.target.value);
+    }
+
     return (
         <section>
-            <input type = "text" placeholder = "Search for a movie..." />
+            <input 
+            type = "text" 
+            placeholder = "Search for a movie..." 
+            value = {searchTerm}
+            onChange = {handleChange}
+            />
             <button> Search </button>
+        
+        <p>You typed: {searchTerm}</p>
         </section>
     );
 }
